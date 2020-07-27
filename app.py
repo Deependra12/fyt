1eb6e6f58f539ebf0e0f70f254f2c3616eff4617
pshreyam <sp02434318@student.ku.edu.np>
2020-07-27 13:24:05 +0545

    Test routes

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
+@app.route('/tutors')
+def teachers():
+    return "Tutors"
 
+@app.route('/students')
+def students():
+    return "Students"
 
 if __name__=='__main__':
     app.run(debug=True)
