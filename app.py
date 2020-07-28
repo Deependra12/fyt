4a197c62d3605903b6c1d7ef61cc9233dbbc0c1a
pshreyam <sp02434318@student.ku.edu.np>
2020-07-28 10:55:31 +0545

    Tested routes (errorhandler)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, jsonify, request 
 
 app = Flask(__name__)
 
@@ -8,13 +8,31 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/tutors')
-def teachers():
-    return "Tutors"
+@app.route('/a/login', methods=['GET', 'POST'])
+def admin_login():
+    ''' Route for admin login '''
+    if request.method == "POST":
+        # replace with WTF forms later on
+        login_id = request.form.get('login-id', '')
+        login_password = request.form.get('login-password', '')
+        print(login_id, login_password)
+        return redirect(url_for('admin_login'))
+    return render_template('admin-login.html')
+
+@app.route('/tutors/login')
+def tutor_home():
+    return "Tutors login"
 
 @app.route('/students')
-def students():
-    return "Students"
+def students_home():
+    return "Students login"
+
+@app.errorhandler(404)
+def error_handler(e):
+    ''' For Handling 404 error '''
+    #replace this with a page for 404 error
+    return "<h1>Sorry, URL not found!</h1>"
+
 
 if __name__=='__main__':
     app.run(debug=True)
