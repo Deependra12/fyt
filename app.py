004b08ded90c9ccfa5e6f670b9313051bd050f54
pshreyam <sp02434318@student.ku.edu.np>
2020-07-29 21:44:29 +0545

    db

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, redirect, url_for, jsonify, request, flash
+from flask import (
+    Flask,
+    render_template, 
+    redirect, 
+    url_for, 
+    jsonify, 
+    request, 
+    flash,
+    session
+)
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Guptaji the great'
 
 
@@ -29,12 +40,9 @@ def login():
     ''' Route for tutor/student login '''
     form = LoginForm()
     if form.validate_on_submit:
-        # replace with WTF forms later on
-        # login_id = request.form.get('login-id', '')
-        # login_password = request.form.get('login-password', '')
-        # print(login_id, login_password)
-        # return redirect(url_for('login'))
-        if form.email.data =='check@gmail.com' and form.password.data == 'guptaji':
+        if form.email.data == 'check@gmail.com' and form.password.data == 'guptaji':
+            session.user = form.email.data
+            print (session.user)
             flash('Login successful','success')
             return redirect(url_for('home'))
         else:
@@ -44,12 +52,14 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def user_register():
+    ''' Route for user registration '''
     form = RegistrationForm() 
     if form.validate_on_submit():
         flash('Your account was created!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
         
+
 @app.errorhandler(404)
 def error_handler(e):
     ''' For Handling 404 error '''
