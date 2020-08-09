935fc8ed9c3cea116e243fd612efc1d0b5a140cf
Bishal Joshi <bishaljoshi08@gmail.com>
2020-08-09 17:08:10 +0545

    flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from  flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-login_manager=LoginManager(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fyt.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../fyt.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Guptaji the great'
 
 db = SQLAlchemy(app)
+login_manager=LoginManager(app)
+migrate = Migrate(app,db)
 
 from . import routes
 from . import models
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from . import db
 
 # models go here
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), index = True, unique = True)
+    hash_password = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Email {}>'.format(self.email)
+
+    def set_password(self, password):
+        self.hash_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hash_password , password)   
