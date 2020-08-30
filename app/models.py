6c7c280392b9faf37e5ad8d684c3382a284941a4
Bishal Joshi <bishaljoshi08@gmail.com>
2020-08-30 15:58:31 +0545

    made location table linked to user

--- app/models.py
+++ app/models.py
@@ -12,7 +12,8 @@ class User(UserMixin, db.Model):
     role = db.Column(db.String(7), index=True)
     student = db.relationship('Student', backref='base', uselist=False)
     teacher = db.relationship('Tutor', backref='base', uselist=False)
-
+    location = db.relationship('Location', backref='User', uselist=False)
+    
     def __repr__(self):
         return '<Email {}>'.format(self.email)
 
@@ -25,10 +26,26 @@ class User(UserMixin, db.Model):
 class Student(db.Model):
     phone = db.Column(db.Integer)
     user_id = db.Column(db.Integer ,  db.ForeignKey('user.id'), primary_key = True ,)
+    full_name = db.Column(db.String(64))
+    guardian_name = db.Column(db.String(64))
+    guardian_address = db.Column(db.String(64))
+    guardian_phone = db.Column(db.Integer)
 
 class Tutor(db.Model):
     phone = db.Column(db.Integer)
     user_id = db.Column(db.Integer ,  db.ForeignKey('user.id'), primary_key = True ,)
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    travel_distance = db.Column(db.String(64))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    address = db.Column(db.String(64))
+    state = db.Column(db.String(64))
+    district = db.Column(db.String(64))
+    municipality = db.Column(db.String(64))
+    ward_no = db.Column(db.Integer)
+    user_id = db.Column(db.Integer , db.ForeignKey('user.id'))
 
 class UserView(ModelView):
     form_columns = ['username', 'email', 'role']
